bd3498ebc1171b5a36c2a3d0751001e487c648f9
Adam Freemer <a@freemer.com>
2016-08-22 20:41:27 -0400

    Updated path for audio file play.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -11,18 +11,22 @@ class MainController < ApplicationController
 
   def show
     root_url = 'https://s3.amazonaws.com'
-    path = '/ab-content/guitar_amp/'
-    count = 1
+    path = '/townsend-labs-ab-audio-assets/'
+    @count = 1
     @experience = Experience.find(params[:id])
-    @clips = []
-    @experience.clips.each do |clip_data|
-      clip = "{title:'#{@experience.name} option #{count}', #{clip_data.format}:'#{root_url}#{path}#{clip_data.file_name}.#{clip_data.format}'}"
-      @clips = "#{@clips}" + "," + "#{clip}"
-      count += 1
+    clips = get_clips
+    @clip_data = []
+    clips.each do |clip_data|
+      clip = "{title:'#{@experience.name} clip #{@count}', #{clip_data.format}:'#{root_url}#{path}#{clip_data.file_name}.#{clip_data.format}'}"
+      @clip_data = "#{@clip_data}" + "," + "#{clip}"
+      @count += 1
     end
-    @clips = @clips.gsub("[],","")
+    @clip_data = @clip_data.gsub("[],","")
     @options = [*1..10]
     @vote = Vote.new
+    @clip_vote = clips
+
+
     respond_to do |format|
        format.js {render layout: false}
     end
@@ -36,5 +40,13 @@ class MainController < ApplicationController
 
   def set_experience
     @experience = Experience.find(params[:id])
+  end
+
+  def get_clips
+    if rand(10) > 5
+      clips = @experience.clips.order("id ASC")
+    else
+      clips = @experience.clips.order("id DESC")
+    end
   end
 end
