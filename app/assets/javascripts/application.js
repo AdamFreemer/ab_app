adcf7d43383c91638d922d5325f5687c31affdb1
Adam Freemer <a@freemer.com>
2016-08-29 18:04:45 -0400

    Layout and mobile first layout testing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require main
 //= require jquery_ujs
 //= require turbolinks
 //= require jplayer.playlist.min.js
